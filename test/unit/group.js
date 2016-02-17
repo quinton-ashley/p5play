2b6bfc7fd2d8d784657e93e218bee5d231ab9585
Atul Varma <varmaa@gmail.com>
2016-02-17 07:24:29 -0500

    Make tests use p5 instance mode.

--- test/unit/group.js
+++ test/unit/group.js
@@ -1,21 +1,31 @@
 describe('Group', function() {
+  var pInst;
+
+  beforeEach(function() {
+    pInst = new p5(function() {});
+  });
+
+  afterEach(function() {
+    pInst.remove();
+  });
+
   describe("removeSprites()", function() {
     it("should remove all sprites", function() {
-      var group = new Group();
+      var group = new pInst.Group();
 
       expect(group.size()).to.equal(0);
-      expect(allSprites.size()).to.equal(0);
+      expect(pInst.allSprites.size()).to.equal(0);
 
-      group.add(createSprite(1, 1));
-      group.add(createSprite(2, 2));
+      group.add(pInst.createSprite(1, 1));
+      group.add(pInst.createSprite(2, 2));
 
       expect(group.size()).to.equal(2);
-      expect(allSprites.size()).to.equal(2);
+      expect(pInst.allSprites.size()).to.equal(2);
 
       group.removeSprites();
 
       expect(group.size()).to.equal(0);
-      expect(allSprites.size()).to.equal(0);
+      expect(pInst.allSprites.size()).to.equal(0);
     });
   });
 });
--- test/unit/spritesheet.js
+++ test/unit/spritesheet.js
@@ -1,22 +1,24 @@
 describe('SpriteSheet', function() {
-  var _warn;
+  var pInst, _warn;
 
   beforeEach(function () {
+    pInst = new p5(function() {});
     _warn = sinon.stub(p5.prototype, '_warn');
   });
 
   afterEach(function () {
     _warn.restore();
+    pInst.remove();
   });
 
   it('does not log warning when created with p5 instance', function() {
-    new SpriteSheet(new p5(function() {}));
+    new pInst.SpriteSheet(pInst);
 
     expect(_warn.callCount).to.equal(0);
   });
 
   it('logs warning when created without p5 instance', function() {
-    new SpriteSheet();
+    new pInst.SpriteSheet();
 
     expect(_warn.callCount).to.equal(1);
     expect(_warn.firstCall.args[0]).to.equal(
