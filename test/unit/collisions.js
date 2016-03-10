0e56a9a1a47bec31c66fb9af348f37e82071aefe
Brad Buchanan <bradley.c.buchanan@gmail.com>
2016-03-10 15:18:52 -0800

    Add group.bounce(sprite) tests, broken with legitimate bug

--- test/unit/collisions.js
+++ test/unit/collisions.js
@@ -1210,5 +1210,62 @@ describe('collisions', function() {
         });
       });
     });
+
+    describe('bounce(sprite)', function () {
+      it('false if no sprites overlap', function () {
+        expect(groupAB.bounce(spriteC)).to.be.false;
+      });
+
+      it('false if no sprite in group overlaps target sprite', function () {
+        moveAToB(spriteA, spriteB);
+        expect(groupAB.bounce(spriteC)).to.be.false;
+      });
+
+      it('true if all sprites in group overlap target sprite', function () {
+        moveAToB(spriteA, spriteC);
+        moveAToB(spriteB, spriteC);
+        expect(groupAB.bounce(spriteC)).to.be.true;
+      });
+
+      describe('true if any sprites in group overlap target sprite', function () {
+        it('A overlaps C', function () {
+          moveAToB(spriteA, spriteC);
+          expect(groupAB.bounce(spriteC)).to.be.true;
+        });
+
+        it('B overlaps C', function () {
+          moveAToB(spriteB, spriteC);
+          expect(groupAB.bounce(spriteC)).to.be.true;
+        });
+      });
+
+      it('does not call callback when not overlapping sprite', function () {
+        groupAB.bounce(spriteC, testCallback);
+        expect(callCount).to.equal(0);
+      });
+
+      describe('passes collider and collidee to callback for each overlap', function () {
+        it('A-C', function () {
+          moveAToB(spriteA, spriteC);
+          groupAB.bounce(spriteC, testCallback);
+          expect(pairs).to.deep.equal([[spriteA.name, spriteC.name]]);
+        });
+
+        it('B-C', function () {
+          moveAToB(spriteB, spriteC);
+          groupAB.bounce(spriteC, testCallback);
+          expect(pairs).to.deep.equal([[spriteB.name, spriteC.name]]);
+        });
+
+        it('A-B-C', function () {
+          moveAToB(spriteA, spriteC);
+          moveAToB(spriteB, spriteC);
+          groupAB.bounce(spriteC, testCallback);
+          expect(pairs).to.deep.equal([
+            [spriteA.name, spriteC.name],
+            [spriteB.name, spriteC.name]]);
+        });
+      });
+    });
   });
 });
