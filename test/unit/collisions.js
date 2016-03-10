808cb57594473da21240d303bdfde96d5f0b7843
Brad Buchanan <bradley.c.buchanan@gmail.com>
2016-03-09 17:47:12 -0800

    Add sprite-group overlap tests.
    To complement the sprite-sprite overlap tests.  These still pass.

--- test/unit/collisions.js
+++ test/unit/collisions.js
@@ -1,8 +1,16 @@
 describe('collisions', function() {
+  const SIZE = 10;
   var pInst;
+  var countingCallback;
 
   beforeEach(function () {
     pInst = new p5(function() {});
+
+    // Count callback calls
+    countingCallback = function () {
+      countingCallback.callCount++;
+    };
+    countingCallback.callCount = 0;
   });
 
   afterEach(function () {
@@ -10,20 +18,12 @@ describe('collisions', function() {
   });
 
   describe('sprite-sprite', function () {
-    const SIZE = 10;
     var spriteA, spriteB;
-    var countingCallback;
     var argTrackingCallback;
 
     beforeEach(function () {
       spriteA = pInst.createSprite(0, 0, SIZE, SIZE);
       spriteB = pInst.createSprite(0, 0, SIZE, SIZE);
-
-      // Count callback calls
-      countingCallback = function () {
-        countingCallback.callCount++;
-      };
-      countingCallback.callCount = 0;
 
       // Remember last arguments to callback
       argTrackingCallback = function () {
@@ -82,6 +82,111 @@ describe('collisions', function() {
         expect(argTrackingCallback.lastArgs.length).to.be.at.least(2);
         expect(argTrackingCallback.lastArgs[0]).to.equal(spriteB);
         expect(argTrackingCallback.lastArgs[1]).to.equal(spriteA);
+      });
+    });
+  });
+
+  describe('sprite-group', function () {
+    var spriteA, spriteB, spriteC, spriteD;
+    var groupA;
+    var argTrackingCallback;
+
+    beforeEach(function () {
+      spriteA = pInst.createSprite(0, 0, SIZE, SIZE);
+      spriteB = pInst.createSprite(0, 0, SIZE, SIZE);
+      spriteC = pInst.createSprite(0, 0, SIZE, SIZE);
+
+      groupA = new pInst.Group();
+      groupA.add(spriteB);
+      groupA.add(spriteC);
+
+      // Remember last arguments to callback
+      argTrackingCallback = function () {
+        argTrackingCallback.lastArgs = arguments;
+      };
+    });
+
+    describe('overlap', function () {
+      it('returns true if sprite overlaps every sprite in group', function () {
+        expect(spriteA.overlap(groupA)).to.be.true;
+      });
+
+      it('returns true if sprite overlaps first sprite in group', function () {
+        spriteC.position.x += 2 * SIZE;
+        expect(spriteA.overlap(groupA)).to.be.true;
+      });
+
+      it('returns true if sprite overlaps last sprite in group', function () {
+        spriteB.position.x += 2 * SIZE;
+        expect(spriteA.overlap(groupA)).to.be.true;
+      });
+
+      it('returns false if sprite does not overlap any sprites in group', function () {
+        spriteB.position.x += 2 * SIZE;
+        spriteC.position.x += 2 * SIZE;
+        expect(spriteA.overlap(groupA)).to.be.false;
+      });
+
+      it('sprite in group can overlap with other sprites in group', function () {
+        expect(spriteB.overlap(groupA)).to.be.true;
+      });
+
+      it('sprite in group does not overlap self', function () {
+        spriteC.position.x += 2 * SIZE;
+        expect(spriteB.overlap(groupA)).to.be.false;
+      });
+
+      it('calls callback once for each overlapping sprite', function () {
+        expect(countingCallback.callCount).to.equal(0);
+        spriteA.overlap(groupA, countingCallback);
+        expect(countingCallback.callCount).to.equal(2);
+
+        countingCallback.callCount = 0;
+        spriteB.position.x += 2 * SIZE;
+        spriteA.overlap(groupA, countingCallback);
+        expect(countingCallback.callCount).to.equal(1);
+
+        countingCallback.callCount = 0;
+        spriteC.position.x += 2 * SIZE;
+        spriteA.overlap(groupA, countingCallback);
+        expect(countingCallback.callCount).to.equal(0);
+      });
+
+      it('passes collider and collidee to callback', function () {
+        spriteA.name = 'spriteA';
+        spriteB.name = 'spriteB';
+        spriteC.name = 'spriteC';
+
+        var pairs;
+        function recordPairs(a, b) {
+          pairs.push([a.name, b.name]);
+        }
+
+        // Overlaps both
+        pairs = [];
+        spriteA.overlap(groupA, recordPairs);
+        expect(pairs).to.deep.equal([[spriteA.name, spriteB.name], [spriteA.name, spriteC.name]]);
+
+        // Overlaps spriteB
+        pairs = [];
+        spriteB.position.x = spriteA.position.x;
+        spriteC.position.x = 2 * SIZE;
+        spriteA.overlap(groupA, recordPairs);
+        expect(pairs).to.deep.equal([[spriteA.name, spriteB.name]]);
+
+        // Overlaps spriteC
+        pairs = [];
+        spriteB.position.x = 2 * SIZE;
+        spriteC.position.x = spriteA.position.x;
+        spriteA.overlap(groupA, recordPairs);
+        expect(pairs).to.deep.equal([[spriteA.name, spriteC.name]]);
+
+        // Overlaps none
+        pairs = [];
+        spriteB.position.x = 2 * SIZE;
+        spriteC.position.x = 2 * SIZE;
+        spriteA.overlap(groupA, recordPairs);
+        expect(pairs).to.deep.equal([]);
       });
     });
   });
