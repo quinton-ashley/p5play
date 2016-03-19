2880a754703337689f31248535116d14202d7903
Brad Buchanan <bradley.c.buchanan@gmail.com>
2016-03-18 17:45:06 -0700

    New broken group test

--- test/unit/group.js
+++ test/unit/group.js
@@ -1,18 +1,48 @@
 describe('Group', function() {
-  var pInst;
+  var pInst, group;
 
   beforeEach(function() {
     pInst = new p5(function() {});
+    group = new pInst.Group();
   });
 
   afterEach(function() {
     pInst.remove();
   });
 
+  it("is an array", function () {
+    expect(Array.isArray(group));
+  });
+
+  describe("add()", function () {
+    it("adds a sprite to the group", function () {
+      var sprite = pInst.createSprite();
+      group.add(sprite);
+      expect(group).to.include(sprite);
+      expect(group[0]).to.equal(sprite);
+      expect(group.contains(sprite)).to.be.at.least(0);
+    });
+
+    it("lets you add different sprites to the group", function () {
+      var sprite1 = pInst.createSprite();
+      var sprite2 = pInst.createSprite();
+      group.add(sprite1);
+      group.add(sprite2);
+      expect(group.length).to.equal(2);
+    });
+
+    it("ignores double addition of a unique sprite", function () {
+      var sprite = pInst.createSprite();
+      group.add(sprite);
+      group.add(sprite);
+      expect(group.length).to.equal(1);
+      expect(group[0]).to.equal(sprite);
+      expect(group[1]).to.be.undefined;
+    });
+  });
+
   describe("removeSprites()", function() {
     it("should remove all sprites", function() {
-      var group = new pInst.Group();
-
       expect(group.size()).to.equal(0);
       expect(pInst.allSprites.size()).to.equal(0);
 
