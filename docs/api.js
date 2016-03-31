c1d78b74477d3f4c64370b227c235f681ebd37a2
Atul Varma <varmaa@gmail.com>
2016-03-31 17:53:36 -0400

    Add docs, examples, update p5.play.js.

---
+++ docs/api.js
@@ -0,0 +1,23 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Animation",
+        "Camera",
+        "Group",
+        "Sprite",
+        "SpriteSheet",
+        "p5.play"
+    ],
+    "modules": [
+        "p5.play"
+    ],
+    "allModules": [
+        {
+            "displayName": "p5.play",
+            "name": "p5.play",
+            "description": "p5.play is a library for p5.js to facilitate the creation of games and gamelike\nprojects.\n\nIt provides a flexible Sprite class to manage visual objects in 2D space\nand features such as animation support, basic collision detection\nand resolution, mouse and keyboard interactions, and a virtual camera.\n\np5.play is not a box2D-derived physics engine, it doesn't use events, and it's\ndesigned to be understood and possibly modified by intermediate programmers.\n\nSee the examples folder for more info on how to use this library."
+        }
+    ],
+    "elements": []
+} };
+});
