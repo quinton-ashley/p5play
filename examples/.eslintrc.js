176ec69a583ee027279372ee509033ef3a78dd7b
Brad Buchanan <bradley.c.buchanan@gmail.com>
2016-03-30 17:35:59 -0700

    Allow implicit global scope

--- examples/.eslintrc.js
+++ examples/.eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   "rules": {
+    // Let examples declare variables in the global scope - they aren't actually
+    // run in global scope, so it's okay.
+    "no-implicit-globals": "off",
+
     // Don't require variables to be defined before they're used, because these
     // examples run in a context where all p5 members are available globally,
     // and it would be a real pain to call them all out as globals here.
