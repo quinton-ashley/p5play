d92d806b2dd909a826ce07b40c8f3127b28c1b9e
Brad Buchanan <bradley.c.buchanan@gmail.com>
2016-03-30 17:07:24 -0700

    Lint tests, with custom config (not passing yet)

---
+++ test/unit/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "env": {
+    "mocha": true
+  },
+  "globals": {
+    "expect": true,
+    "p5": true
+  },
+  "rules": {
+
+    // Allow empty functions in tests, because we often want to pass a no-op
+    // placeholder when testing.
+    // Very frequent: new p5(function () {})
+    "no-empty-function": "off",
+
+    // Chai BDD assertions violate this style rule, so disable it in tests
+    // e.g. expect(value).to.equal('something')
+    "no-unused-expressions": "off"
+  }
+};
