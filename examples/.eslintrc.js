addc514f14ba370c11fd5e7bf6a80501c534628a
Brad Buchanan <bradley.c.buchanan@gmail.com>
2016-03-30 17:33:22 -0700

    Lint examples, with custom config (not passing yet)

---
+++ examples/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "rules": {
+    // Don't require variables to be defined before they're used, because these
+    // examples run in a context where all p5 members are available globally,
+    // and it would be a real pain to call them all out as globals here.
+    "no-undef": "off",
+
+    // Likewise, allow unused functions/variables because certain functions like
+    // setup() and draw() get called by global-mode p5.play.
+    "no-unused-vars": "off"
+  }
+};
