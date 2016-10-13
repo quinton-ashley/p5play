a434f6a769dd7174d05d61b80a0ca0657b5fe2e7
Brad Buchanan <bradley.c.buchanan@gmail.com>
2016-10-13 12:21:15 -0700

    Move sprite.bounce() tests to own file

--- test/unit/collisions.js
+++ test/unit/collisions.js
@@ -66,55 +66,6 @@ describe('collisions', function() {
     pInst.remove();
   });
 
-  describe('sprite.bounce(sprite)', function() {
-    it('false if sprites do not overlap', function() {
-      expect(spriteA.bounce(spriteB)).to.be.false;
-      expect(spriteB.bounce(spriteA)).to.be.false;
-    });
-
-    it('true if sprites overlap', function() {
-      moveAToB(spriteA, spriteB);
-      expect(spriteA.bounce(spriteB)).to.be.true;
-
-      moveAToB(spriteA, spriteB);
-      expect(spriteB.bounce(spriteA)).to.be.true;
-    });
-
-    it('calls callback once if sprites overlap', function() {
-      expect(callCount).to.equal(0);
-
-      moveAToB(spriteA, spriteB);
-      spriteA.bounce(spriteB, testCallback);
-      expect(callCount).to.equal(1);
-
-      moveAToB(spriteA, spriteB);
-      spriteB.bounce(spriteA, testCallback);
-      expect(callCount).to.equal(2);
-    });
-
-    it('does not call callback if sprites do not overlap', function() {
-      expect(callCount).to.equal(0);
-      spriteA.bounce(spriteB, testCallback);
-      expect(callCount).to.equal(0);
-      spriteB.bounce(spriteA, testCallback);
-      expect(callCount).to.equal(0);
-    });
-
-    describe('passes collider and collidee to callback', function() {
-      it('A-B', function() {
-        moveAToB(spriteA, spriteB);
-        spriteA.bounce(spriteB, testCallback);
-        expect(pairs).to.deep.equal([[spriteA.name, spriteB.name]]);
-      });
-
-      it('B-A', function() {
-        moveAToB(spriteA, spriteB);
-        spriteB.bounce(spriteA, testCallback);
-        expect(pairs).to.deep.equal([[spriteB.name, spriteA.name]]);
-      });
-    });
-  });
-
   describe('sprite.bounce(group)', function() {
     it('false if sprite does not overlap any sprites in group', function() {
       expect(spriteA.bounce(groupCD)).to.be.false;
