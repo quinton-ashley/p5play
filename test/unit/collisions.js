500afd5406cc36ad3c281822771106356667f481
Brad Buchanan <bradley.c.buchanan@gmail.com>
2016-10-13 11:52:24 -0700

    Move sprite.collide(sprite) tests to own file

--- test/unit/collisions.js
+++ test/unit/collisions.js
@@ -323,55 +323,6 @@ describe('collisions', function() {
     });
   });
 
-  describe('sprite.collide(sprite)', function() {
-    it('false if sprites do not overlap', function() {
-      expect(spriteA.collide(spriteB)).to.be.false;
-      expect(spriteB.collide(spriteA)).to.be.false;
-    });
-
-    it('true if sprites overlap', function() {
-      moveAToB(spriteA, spriteB);
-      expect(spriteA.collide(spriteB)).to.be.true;
-
-      moveAToB(spriteA, spriteB);
-      expect(spriteB.collide(spriteA)).to.be.true;
-    });
-
-    it('calls callback once if sprites overlap', function() {
-      expect(callCount).to.equal(0);
-
-      moveAToB(spriteA, spriteB);
-      spriteA.collide(spriteB, testCallback);
-      expect(callCount).to.equal(1);
-
-      moveAToB(spriteA, spriteB);
-      spriteB.collide(spriteA, testCallback);
-      expect(callCount).to.equal(2);
-    });
-
-    it('does not call callback if sprites do not overlap', function() {
-      expect(callCount).to.equal(0);
-      spriteA.collide(spriteB, testCallback);
-      expect(callCount).to.equal(0);
-      spriteB.collide(spriteA, testCallback);
-      expect(callCount).to.equal(0);
-    });
-
-    describe('passes collider and collidee to callback', function() {
-      it('A-B', function() {
-        moveAToB(spriteA, spriteB);
-        spriteA.collide(spriteB, testCallback);
-        expect(pairs).to.deep.equal([[spriteA.name, spriteB.name]]);
-      });
-
-      it('B-A', function() {
-        moveAToB(spriteA, spriteB);
-        spriteB.collide(spriteA, testCallback);
-        expect(pairs).to.deep.equal([[spriteB.name, spriteA.name]]);
-      });
-    });
-  });
-
   describe('sprite.collide(group)', function() {
     it('false if sprite does not overlap any sprites in group', function() {
       expect(spriteA.collide(groupCD)).to.be.false;
